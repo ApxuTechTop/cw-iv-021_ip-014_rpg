e146514e7dce4093280a34cd1fab74b41f07cbd2
ApxuTechTop <78413120+ApxuTechTop@users.noreply.github.com>
2021-04-17 18:41:49 +0600

    Update build params

--- builder/build.android.lua
+++ builder/build.android.lua
@@ -1,21 +1,24 @@
-os.execute("echo %CD% > tmp.txt")
-local f = io.open("tmp.txt", "r")
-local cwd = f:read("*l")
-io.close(f)
-os.remove("tmp.txt")
-cwd = cwd:sub(1, cwd:len()-1)
-local params =
-{
-	platform='Android',
-	appName='Similarity',
-	appVersion='1.0',
-	dstPath=cwd ..'/bin',
-	sdkPath='C:/Users/ApxuTechTop/AppData/Roaming/Corona Labs/Corona Simulator/Android Build/sdk',
-	projectPath=cwd ..'/Similarity',
-	androidAppPackage='com.coronalabs.Similarity',
-	
-	-- Following are optional:
-	--[[
+local getenv = function(name)
+    os.execute("echo %" .. name .. "% > tmp.txt")
+    local f = io.open("tmp.txt", "r")
+    local value = f:read("*l")
+    io.close(f)
+    os.remove("tmp.txt")
+    return string.match(value, ".+[^ ]")
+end
+local cwd = getenv("CD")
+local sdk = getenv("ANDROID_SDK_ROOT")
+local params = {
+    platform = 'Android',
+    appName = 'Similarity',
+    appVersion = '1.0',
+    dstPath = cwd .. '/bin',
+    sdkPath = sdk,
+    projectPath = cwd .. '/Similarity',
+    androidAppPackage = 'com.coronalabs.Similarity'
+
+    -- Following are optional:
+    --[[
 	certificatePath='/path/to/cert.keystore',
 	keystorePassword='',
 	keystoreAlias='',
