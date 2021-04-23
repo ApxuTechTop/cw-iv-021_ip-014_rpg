45a423edd00edaea1e94f22ca1c3b3cde4644ce9
Avili0 <78966423+Avili0@users.noreply.github.com>
2021-04-23 17:43:43 +0600

    Format config.lua

--- Similarity/config.lua
+++ Similarity/config.lua
@@ -2,22 +2,19 @@
 -- For more information on config.lua see the Project Configuration Guide at:
 -- https://docs.coronalabs.com/guide/basics/configSettings
 --
+application = {
+    content = {
+        width = 720,
+        height = 1520,
+        scale = "letterbox",
+        fps = 60
 
-application =
-{
-	content =
-	{
-		width = 720,
-		height = 1520, 
-		scale = "letterbox",
-		fps = 60,
-		
-		--[[
+        --[[
 		imageSuffix =
 		{
 			    ["@2x"] = 2,
 			    ["@4x"] = 4,
 		},
 		--]]
-	},
+    }
 }
