be420f8c822ac81b5ad506fbecf0f0b634744604
Avili0 <78966423+Avili0@users.noreply.github.com>
2021-04-23 17:44:53 +0600

    Update World.lua

--- Similarity/World.lua
+++ Similarity/World.lua
@@ -18,58 +18,73 @@ local lootMeta = {
             elseif type(item) == "number" then
                 table.remove(self.items, i)
             else
-                error("Expected number or table", 1)
+                assert(false, "Expected number or table")
             end
         end
     }
 }
 
 local battleMeta = {
-    addEntity = function(self, entity, key)
-        if key ~= "right" and key ~= "left" then
-            error("Expected 'right' or 'left' key")
-        end
-        self[key][#self[key] + 1] = entity
-        -- ивент на отображение
-    end,
-    removeEntity = function(self, entity, key)
-        if key and key ~= "right" and key ~= "left" then
-            error("Expected 'right' or 'left' key")
-        end
-        if key then
-            if type(entity) == "table" then
-                for i = 1, #self[key] do
-                    if self[key][i] == entity then
-                        table.remove(self[key], i)
-                    end
-                end
-            elseif type(entity) == "number" then
-                table.remove(self[key], i)
-            else
-                error("Expected number or table", 1)
+    __index = {
+
+        addEntity = function(self, entity, key)
+            if key ~= "right" and key ~= "left" then
+                assert(false, "Expected 'right' or 'left' key")
             end
-        else
-            for _, key in pairs({"left", "right"}) do
-                if type(entity) == "table" then
+            self[key][#self[key] + 1] = entity
+            entity.battle = self
+            -- ивент на отображение
+        end,
+        removeEntity = function(self, entity, key)
+            if key and key ~= "right" and key ~= "left" then
+                assert(false, "Expected 'right' or 'left' key")
+            end
+            for _, key in pairs(key and {key} or {"left", "right"}) do
+                local num
+                if type(entity) == "number" then
+                    num = entity
+                else
                     for i = 1, #self[key] do
                         if self[key][i] == entity then
-                            table.remove(self[key], i)
+                            num = i
+                            break
                         end
                     end
-                elseif type(entity) == "number" then
-                    table.remove(self[key], i)
-                else
-                    error("Expected number or table", 1)
+                end
+                if num then
+                    print(key, num)
+                    self[key][num].battle = nil
+                    table.remove(self[key], num)
+                    if #self[key] == 0 then
+                        local enemySide = (key == "left") and "right" or "left"
+                        for i = #self[enemySide], 1, -1 do
+                            self:removeEntity(i, enemySide)
+                        end
+                    end
+                    return true
                 end
             end
+        end,
+        run = function(self)
+            for _, entity in pairs(self.left) do
+                entity:think()
+                entity.battleBuffer:run()
+            end
+            for _, entity in pairs(self.right) do
+                entity:think()
+                entity.battleBuffer:run()
+            end
         end
-    end
+    }
 }
 
 local locationMeta = {
     __index = {
-        addEntity = function(self, entity)
+        addEntity = function(self, entity, position)
+            self.entities = self.entities or {}
+            entity.position = position or {loc = self, x = 0, y = 0}
             self.entities[#self.entities + 1] = entity
+
             -- ивент на отображение
         end,
         removeEntity = function(self, entity)
@@ -77,12 +92,11 @@ local locationMeta = {
                 for i = 1, #self.entities do
                     if self.entities[i] == entity then
                         table.remove(self.entities, i)
+                        return true
                     end
                 end
-            elseif type(entity) == "number" then
-                table.remove(self.entities, i)
             else
-                error("Expected number or table", 1)
+                assert(false, "Expected table")
             end
         end,
         addBattle = function(self, battle)
@@ -99,7 +113,7 @@ local locationMeta = {
             elseif type(battle) == "number" then
                 table.remove(self.battles, i)
             else
-                error("Expected number or table", 1)
+                assert(false, "Expected table")
             end
         end,
         addLoot = function(self, loot)
@@ -134,7 +148,7 @@ local locationMeta = {
             elseif type(loot) == "number" then
                 table.remove(self.loot, i)
             else
-                error("Expected number or table", 1)
+                assert(false, "Expected number or table")
             end
         end,
         newLoot = function(self, options)
@@ -142,81 +156,138 @@ local locationMeta = {
             setmetatable(loot, lootMeta);
             return loot
         end,
-        newBattle = function(options)
-            local battle = {pos = options.pos or {0.5, 0.5}, left = options.left, right = options.right}
+        newBattle = function(self, options)
+            local battle = {
+                position = options.position or {loc = self, x = 50, y = 50},
+                left = options.left,
+                right = options.right
+            }
+            battle.position.loc = battle.position.loc or self
             setmetatable(battle, battleMeta)
+            self.battles = self.battles or {}
+            self.battles[#self.battles + 1] = battle
+            for _, entity in pairs(battle.left) do
+                entity.battle = battle
+            end
+            for _, entity in pairs(battle.right) do
+                entity.battle = battle
+            end
             return battle
         end
     }
 }
 
-World.newLocation = function(options)
-    local location = {
-        id = options.id,
-        name = options.name,
-        desc = options.desc,
-        texture = options.texture,
-        path = options.path,
-        loot = options.loot,
-        entities = options.entities,
-        battles = options.battles
+local worldMeta = {
+    __index = {
+        clear = function(self, entity)
+            local location = entity.position.loc
+            if not location:removeEntity(entity) then
+                assert(false, "Didn't find entity")
+            end
+            for k, v in pairs(location.battles) do
+                if v:removeEntity(entity) then
+                    break
+                end
+            end
+            for k, battleAction in pairs(entity.battleBuffer) do
+                if battleAction.timer then
+                    timer.cancel(battleAction.timer)
+                end
+                if battleAction.another.timer then
+                    timer.cancel(battleAction.another.timer)
+                end
+            end
+            return true
+        end,
+        newLocation = function(self, options)
+            local location = {
+                id = options.id,
+                name = options.name,
+                desc = options.desc,
+                texture = options.texture,
+                path = options.path,
+                loot = options.loot,
+                entities = options.entities,
+                battles = options.battles,
+                world = self
+            }
+            setmetatable(location, locationMeta)
+            self.locations[location.id] = location
+            return location
+        end
     }
-    setmetatable(location, locationMeta);
-
-    return location
-end
+}
 
 World.new = function()
-    local world = {}
-
+    local world = {locations = {}}
+    setmetatable(world, worldMeta)
     return world
 end
-
+return World
 --[=[
 
-    location = {
-        id = "string",
-        name = "string",
-        desc = function(world)
-            return "Description"
-        end,
-        texture = function()  end,
-        path = {{id = "string", pos = {1, 0.5}, image = "string"}},
-        entities = {},
-        loot = {{items = {item}, pos = {0.2, 0.2}, obscurity = -1}},
-        battles = {battle}
-    }
-
-    world = {
-        time = {
-            y = 1000, 
-            m = 1, 
-            d = 1, 
-            h = 1
+location = {
+    id = "string",
+    height=100,
+    width=100,
+    name = "string",
+    desc = function(world)
+        return "Description"
+    end,
+    texture = function()
+    end,
+    path = {{id = "string", pos = {1, 0.5}, image = "string", graphics = gui.icon}},
+    entities = {entity},
+    loot = {{items = {item}, pos = {0.2, 0.2}, obscurity = -1}},
+    battles = {battle},
+    world = world,
+    graphics = {
+        texture = display.object,
+        info = {
+            name = display.text, 
+            desc = display.text, 
+            minimap = display.container, 
+            loot = display.object
         }
-        weather = {},
-        locations = {location}
     }
+}
 
-    battle = {
-        left = {entity.id},
-        right = {entity.id},
-        pos = {0.3, 0.3}
-    }
+world = {
+    time = {
+        y = 1000, 
+        m = 1, 
+        d = 1, 
+        h = 1
+    }, 
+    weather = {}, 
+    locations = {location}, 
+    graphics = widget.scrollView
+}
 
-    function findItems()
-        local l = world.locations[self.position[1]]
-        for key, loot in pairs(l.loot) do
-            if (obscurity == -1) then
+battle = {
+    left = {entity},
+    right = {entity},
+    pos = {
+        loc = location,
+        x = 300,
+        y = 350
+    }, 
+    graphics = gui.icon
+}
 
-            end
-            local posx, posy = loot.pos[1], loot.pos[2]
-            local x, y = self.position[2], self.position[3]
-            local dist = math.sqrt((posx - x)^2 + (posy - y)^2)
-            local r = math.random()
-            if (r * obscurity^(0.5) * dist < 1) then
+function findItems()
+    local l = world.locations[self.position[1]]
+    for key, loot in pairs(l.loot) do
+        if (obscurity == -1) then
 
-            end
+        end
+        local posx, posy = loot.pos[1], loot.pos[2]
+        local x, y = self.position[2], self.position[3]
+        local dist = math.sqrt((posx - x) ^ 2 + (posy - y) ^ 2)
+        local r = math.random()
+        if (r * obscurity ^ (0.5) * dist < 1) then
+
         end
     end
+end
 -- ]=]
